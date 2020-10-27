81bce5b8d4047adb5f301905472e26c8b3ed5d64
Alex <format37@gmail.com>
2020-10-27 16:28:08 +0300

    new file:   server.py 	new file:   sql_cook.ipynb

---
+++ server.py
@@ -0,0 +1,22 @@
+import asyncio
+from aiohttp import web
+
+WEBHOOK_PORT = 8081
+WEBHOOK_LISTEN = '0.0.0.0'  # In some VPS you may need to put here the IP addr
+
+async def call_test(request):
+	return web.Response(text='ok',content_type="text/html")
+
+async def call_filenames(request):
+	#question = request.rel_url.query['question']	
+	return web.Response(text='ok',content_type="text/html")
+
+app = web.Application()
+app.router.add_route('GET', '/test', call_test)
+app.router.add_route('GET', '/filenames', call_filenames)
+
+web.run_app(
+    app,
+    host=WEBHOOK_LISTEN,
+    port=WEBHOOK_PORT,
+)
