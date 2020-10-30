90be6057ff47e01c26afbbfb85a197efe4a44939
Alex <format37@gmail.com>
2020-10-30 13:54:56 +0300

    modified:   server.py 	modified:   sql_cook.ipynb

--- server.py
+++ server.py
@@ -19,47 +19,23 @@ async def call_filenames(request):
 	with open(file_path_data, 'w') as source_file: 
 		source_file.write(await request.text())
 		source_file.close()
-		
-	# read data
-	#with open(file_path_data, 'rb') as source_file:
-	#	lines = source_file.readlines()
-	#	source_file.close()
 		#os.unlink(file_path_data) TODO: enable
 
-	df = pd.read_csv(file_path_data,';')
-	'''
-	linkedid = []
-	linkedid.append(
-		{
-		'call_id':'0012388684',
-		'date_from':'2020-10-15 22:15:06',
-		'date_to':'2020-10-15 22:17:06',
-		'linkedid':'1602789350.14400182',
-		}
-	)
-	linkedid.append(
-		{
-		'call_id':'0012388685',
-		'date_from':'2020-10-15 22:18:25',
-		'date_to':'2020-10-15 22:20:25',
-		'linkedid':'1602789550.14400185',
-		}
-	)
-	'''
-	sql_result = cdr_filenames(df)
+		df = pd.read_csv(file_path_data,';',dtype = {'call_id': 'str', 'linkedid': 'str'})
+		sql_result = cdr_filenames(df)
 
-	answer = ''
+		answer = ''
 
-	for row in sql_result:
+		for row in sql_result:
 
-		call_id		= row[0]
-		linkedid	= row[1]
-		filename	= row[2]
+			call_id		= row[0]
+			linkedid	= row[1]
+			filename	= row[2]
 
-		last_slash_pos = filename.rindex('/')
-		filename 	= filename[last_slash_pos+1:]
+			last_slash_pos = filename.rindex('/')
+			filename 	= filename[last_slash_pos+1:]
 
-		answer += call_id+';'+linkedid+';'+filename+';'+'\n'
+			answer += call_id+';'+linkedid+';'+filename+';'+'\n'
 		
 	return web.Response(text=answer,content_type="text/html")
 
